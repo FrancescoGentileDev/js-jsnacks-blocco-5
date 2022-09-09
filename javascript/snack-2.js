affbe2b50c2754e50dcf8a39b8850978b0ee9028
svalvastic <assogames2@gmail.com>
2022-09-09 16:06:17 +0200

    ✨ feat(snack-2): complete

--- javascript/snack-2.js
+++ javascript/snack-2.js
@@ -6,3 +6,29 @@ console.log("snack-2")
  * che hanno la posizione compresa tra i due numeri.
  */
 
+function splitArray(array, min, max) {
+    let newArray = []
+    let temp = 0;
+    if (min > max) {
+        temp = min;
+        min = max
+        max = temp;
+    }
+
+    array.forEach((element, index) => {
+        if (index >= min && index <= max) {
+            newArray.push(element)
+        }
+    });
+
+
+    return newArray;
+}
+
+
+let arrayNumber = [10, 20, 25, 7, 3, 78, 24, 7, 45];
+
+let newNumberArray = splitArray(arrayNumber, 1, 5);
+
+
+console.log(newNumberArray)
